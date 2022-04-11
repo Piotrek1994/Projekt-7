eb1370c3de5bba9f2a2079496288e8198d7c75e3
Piorek1994 <pprus94@gmail.com>
2022-04-11 15:24:10 +0200

    Creating function to complete task

--- script.js
+++ script.js
@@ -17,17 +17,52 @@ const prepareDOMElements = () => {
 }
 const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
 }
 
 const addNewTodo = () => {
 	if (todoInput.value !== '') {
 		newTodo = document.createElement('li')
 		newTodo.textContent = todoInput.value
-        ulList.append(newTodo)
-        todoInput.value = ''
-        erroInfo.textContent = ''
+		createToolsArea()
+
+		ulList.append(newTodo)
+		todoInput.value = ''
+		erroInfo.textContent = ''
 	} else {
 		erroInfo.textContent = 'Wpisz treść zadania'
+	}
+}
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+	newTodo.append(toolsPanel)
+
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		console.log('edit')
+
+	} else if (e.target.matches('.delete')) {
+		console.log('delete')
+   
 	}
 }
 
