f56eb28bda4d53249d869a33958ea1bc5d9666bb
Piorek1994 <pprus94@gmail.com>
2022-04-11 15:37:10 +0200

    Adding popup

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ let addBtn // przycisk ADD
 let ulList // lista zadan, tagi UL
 let newTodo // nowo dodany LI
 
+let popup // popup
+let popupInfo // tekst w popupie
+let todoToEdit // edytywany Todo
+let popupInput // input w popupie
+let popupAddBtn // przycisk "zatwierdz"
+let popupCloseBtn // przycisk "anuluj"
+
 const main = () => {
 	prepareDOMElements()
 	prepareDOMEvents()
@@ -14,10 +21,18 @@ const prepareDOMElements = () => {
 	erroInfo = document.querySelector('.error-info')
 	addBtn = document.querySelector('.btn-add')
 	ulList = document.querySelector('.todolist ul')
+
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
 }
+
 const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTodo)
 	ulList.addEventListener('click', checkClick)
+    popupCloseBtn.addEventListener('click', closePopUp)
 }
 
 const addNewTodo = () => {
@@ -58,12 +73,20 @@ const checkClick = e => {
 	if (e.target.matches('.complete')) {
 		e.target.closest('li').classList.toggle('completed')
 	} else if (e.target.matches('.edit')) {
-		console.log('edit')
-
+		editTodo()
 	} else if (e.target.matches('.delete')) {
 		console.log('delete')
-   
 	}
 }
 
+const editTodo = () => {
+    popup.style.display = 'flex'
+}
+
+const closePopUp = () => {
+    popup.style.display = 'none'
+}
+
+
 document.addEventListener('DOMContentLoaded', main)
+
